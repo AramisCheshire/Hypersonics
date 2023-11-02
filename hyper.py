b400ce32a2e70e0b760a5292e1b92d8fc4915894
AramisCheshire <149718013+AramisCheshire@users.noreply.github.com>
2023-11-02 22:50:20 +0000

    Update hyper.py

--- hyper.py
+++ hyper.py
@@ -1,24 +1,29 @@
 import math
 from math import sin
 from math import cos
+from math import pi
 print("A program to calculate flowfields for hypersonic flows")
 print("Start with property change across an oblique shock")
 print("starting with default flow properties")
 
-#properties = [V (m/s), a (m/s) ,P (kPa) ,p (kg/m3) ,T (K), y]
-flow1 = [3360,336,100,1.225,300, 1.4] #this is an example array of some hypersonic flow at M=10
+#velocity vector = [ Vx, Vy, Vz]
+vel1 = [3360,0,0]
+#props = [V (m/s),a (m/s) ,P (kPa) ,p (kg/m3) ,T (K), y]
+flow1 = [vel1,336,100,1.225,300, 1.4]
 
-
-def oblique(prop,oblang): #this function applies the exact oblique shock equations to a flow and generates the new properties
-	M1 = prop[0]/prop[1]
+def oblique(prop,oblang): 
+	#this function applies the exact oblique shock equations to a flow and generates the new properties
+	Vx = prop[0][0] #reading Vx from V inside of flow1
 	a = prop[1]
-	V1 = prop[0]
+	M1 = Vx/a
 	P1 = prop[2]
 	p1 = prop[3]
 	T1 = prop[4]
 	y = prop[5]
-	B = math.pi*oblang/180
-	#some commonly seen terms
+	B = pi*oblang/180 #converting the shock wave angle from degrees to radians
+	
+	
+	#combining the term (M1^2)*(SinB^2) into one variable to reduce clutter
 	MsB = (M1**2)*((sin(B))**2)
 	
 	
@@ -26,29 +31,25 @@ def oblique(prop,oblang): #this function applies the exact oblique shock equatio
 	P2 = P1*(1+ 2*y*(1/(2*y + 1))*(MsB-1))
 	p2 = p1*((y+1)*MsB)*(1/((y-1)*MsB + 2))
 	T2 = T1*(P2/P1)*(p1/p2)
-	u2 = V1*(1-(2*(MsB-1))*(1/((y+1)*(M1**2))))
-	v2 = V1*2*cos(B)*(MsB-1)*(1/((y+1)*(M1**2)))
-	print(P2)
-	print(p2)
-	print(T2)
-	print(u2)
-	print(v2)
-	checkV = pow(u2**2 + v2**2,0.5)
-	print(checkV)
+	u = Vx*(1-(2*(MsB-1))*(1/((y+1)*(M1**2)))) #note that for input flow we use the notation Vx/Vy/Vz but for output flow we use u,v,w
+	v = Vx*2*cos(B)*(MsB-1)*(1/((y+1)*(M1**2)))
+	w = 0 #in this case the function only considers 2 dimensional flow so w is 0, but we will keep the input format consistent with 3D flow
+
+	 
+	return [[u,v,w],a,P2,p2,T2,y]
 
 
 def oblique_third(a,y):
 #this function takes a list as an argument and adds 5 to the 1st element of that list
-	M1 = prop[0]/prop[1]
+	v1 = prop[0][0] #reading V1 from V inside of flow1
 	a = prop[1]
-	V1 = prop[0]
+	M1 = V1/a
 	P1 = prop[2]
 	p1 = prop[3]
 	T1 = prop[4]
 	y = prop[5]
-	B = math.pi*oblang/180
-
-###################################################
+	B = pi*oblang/180 #converting the shock wave angle from degrees to radians
+	
 
 	P2 = P1 * ( (2 * y / (y + 1) ) * M1 * (sin(B)**2) )
 
@@ -56,16 +57,38 @@ def oblique_third(a,y):
 
 	T2 = T1 * ( 2*y * (y - 1) / ((y + 1)**2) * (M1**2)* (sin(B)**2) )
 
-	u2 = v1 * (1 - ( 2 * (sin(B)**2)/(y+1) ) )
+	u = v1 * (1 - ( 2 * (sin(B)**2)/(y+1) ) )  #note that for input flow we use the notation Vx/Vy/Vz but for output flow we use u,v,w
 
-	v2 = v1 * ( sin(2*B)  / ( y + 1) )
+	v = v1 * ( sin(2*B)  / ( y + 1) )
+
+	w = 0 #in this case the function only considers 2 dimensional flow so w is 0, but we will keep the input format consistent with 3D flow
 
 	Cp = (4 / (y+1)) * (sin(B)**2)
 
-	return([V1,a,P2,p2,T2,y])
+	return [[u,v,w],a,P2,p2,T2,y]
 
 ####################################################
 	
 """---------------main-------------"""
+print("original: ")
 print(flow1)
-oblique(flow1,10)
+print("exact: ")
+flow2_ex = oblique(flow1,10)
+print(flow2_ex)
+print("at limit: ")
+flow2_lim = oblique(flow1,10)
+print(flow2_lim)
+
+
+#some math checks - delete later
+R1 = flow1[3]/(flow1[2]*1000*flow1[4])
+
+R2 = flow2_ex[3]/(flow2_ex[2]*1000*flow2_ex[4])
+print(R1)
+print(R2)
+
+R3 = flow1[3]/(flow1[2]*1000*flow1[4])
+
+R4 = flow2_lim[3]/(flow2_lim[2]*1000*flow2_lim[4])
+print(R3)
+print(R4)
