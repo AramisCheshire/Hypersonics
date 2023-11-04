62a3e2e981d14648463468e3c4b7abb07825a551
AramisCheshire <149718013+AramisCheshire@users.noreply.github.com>
2023-11-04 21:07:17 +0000

    Update hyper.py

--- hyper.py
+++ hyper.py
@@ -12,6 +12,7 @@ vel1 = [3360,0,0]
 flow1 = [vel1,336,100,1.225,300, 1.4]
 
 
+
 #the exact equations for flow property changes across an oblique shock
 def oblique(prop,oblang): 
 	#this function applies the exact oblique shock equations to a flow and generates the new properties
@@ -26,22 +27,39 @@ def oblique(prop,oblang):
 	
 	
 	#combining the term (M1^2)*(SinB^2) into one variable to reduce clutter
-	MsB = (M1**2)*((sin(B))**2)
+	M2B2 = (M1**2)*((sin(B))**2)
 	
 	
-	#calculate p2
-	P2 = P1*(1+ 2*y*(1/(2*y + 1))*(MsB-1))
-	p2 = p1*((y+1)*MsB)*(1/((y-1)*MsB + 2))
+	#pressure
+	numP2 = 2*y*(M2B2 - 1)
+	denP2 = y + 1
+	P2 = P1*(1+(numP2/denP2))
+	
+	#density
+	nump2 = (y+1)*M2B2
+	denp2 = (y-1)*M2B2
+	p2 = p1*nump2/denp2
+	
+	#temperature
 	T2 = T1*(P2/P1)*(p1/p2)
-	u = Vx*(1-(2*(MsB-1))*(1/((y+1)*(M1**2)))) #note that for input flow we use the notation Vx/Vy/Vz but for output flow we use u,v,w
-	v = Vx*2*cos(B)*(MsB-1)*(1/((y+1)*(M1**2)))
-	w = 0 #in this case the function only considers 2 dimensional flow so w is 0, but we will keep the input format consistent with 3D flow
+	
+	#u2
+	numu2 = 2*(M2B2 - 1)
+	denu2 = (y+1)*(M1**2)
+	u2 = Vx*(1-(numu2/denu2))
+	
+	#v2
+	numv2 = 2*cos(B)*(M2B2 - 1)
+	denv2 = (y+1)*(M1**2)
+	v2 = Vx*(numv2)*(denv2)
+	
+	w2 = 0 #in this case the function only considers 2 dimensional flow so w is 0, but we will keep the input format consistent with 3D flow
 
 	 
-	return [[u,v,w],a,P2,p2,T2,y]
+	return [[u2,v2,w2],a,P2,p2,T2,y]
 
 #the equations for flow property changes across an oblique shock at the hypersonic limit
-def oblique_limit(x,y):
+def oblique_limit(x,obl):
 #this function takes a list as an argument and adds 5 to the 1st element of that list
 	v1 = x[0][0] #reading V1 from V inside of flow1
 	a = x[1]
@@ -50,12 +68,12 @@ def oblique_limit(x,y):
 	p1 = x[3]
 	T1 = x[4]
 	y = x[5]
-	B = pi*y/180 #converting the shock wave angle from degrees to radians
+	B = pi*obl/180 #converting the shock wave angle from degrees to radians
 	
 
-	P2 = P1 * ( (2 * y / (y + 1) ) * M1 * (sin(B)**2) )
+	P2 = P1 * ( (2 * y / (y + 1) ) * (M1**2) * (sin(B)**2) )
 
-	p2 = p1 * ( y + 1 / y - 1 )
+	p2 = p1 * ( (y + 1) / (y - 1) )
 
 	T2 = T1 * ( 2*y * (y - 1) / ((y + 1)**2) * (M1**2)* (sin(B)**2) )
 
@@ -68,6 +86,8 @@ def oblique_limit(x,y):
 	Cp = (4 / (y+1)) * (sin(B)**2)
 
 	return [[u,v,w],a,P2,p2,T2,y]
+
+	
 
 ####################################################
 	
@@ -83,14 +103,14 @@ print(flow2_lim)
 
 
 #some math checks - delete later
-R1 = flow1[3]/(flow1[2]*1000*flow1[4])
+#flow1 = [vel1,336,100,1.225,300, 1.4]
 
-R2 = flow2_ex[3]/(flow2_ex[2]*1000*flow2_ex[4])
+R1 = flow1[2]*1000/(flow1[3]*flow1[4])
+
+R2 = flow2_ex[2]*1000/(flow2_ex[3]*flow2_ex[4])
 print(R1)
 print(R2)
 
-R3 = flow1[3]/(flow1[2]*1000*flow1[4])
 
-R4 = flow2_lim[3]/(flow2_lim[2]*1000*flow2_lim[4])
-print(R3)
+R4 = flow2_lim[2]*1000/(flow2_lim[3]*flow2_lim[4])
 print(R4)
