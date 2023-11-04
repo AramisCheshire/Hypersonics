0d81be82282fbed2a9a9a7a1eed2b5d92a7bd416
AramisCheshire <149718013+AramisCheshire@users.noreply.github.com>
2023-11-04 23:18:28 +0000

    Update angle_graph.py

--- angle_graph.py
+++ angle_graph.py
@@ -2,10 +2,6 @@ from hyper import oblique
 from hyper import oblique_limit
 from hyper import ThetaBeta
 
-#velocity vector = [ Vx, Vy, Vz]
-vel1 = [3360,0,0]
-#props = [V (m/s),a (m/s) ,P (kPa) ,p (kg/m3) ,T (K), y]
-flow1 = [vel1,336,100,1.225,300, 1.4]
 
 #ThetaBeta(M1,Bdeg,y):
 
