95bc7644b6b79d2fc0f30dfabd6574a30f0f8c8a
testudoguy <149730227+testudoguy@users.noreply.github.com>
2023-11-05 14:02:42 +0200

    Update pressure_vs_mach_graph.py

--- pressure_vs_mach_graph.py
+++ pressure_vs_mach_graph.py
@@ -4,16 +4,25 @@ from hyper import oblique_limit
 
 vel1 = [2444, 0, 0]
 flow1 = [vel1, 323, 132, 1.225, 200, 1.4]
-mach_values = [4, 23, 43, 45, 123]
+mach_values = []
 pressure = []
 generated_mach_values = []
-
 tmp = []
+
+
+x = 0
+while x < 40 :
+      x = x + 1
+      mach_values.append(x)
+
+print('Initial_mach_values: ')
+print(mach_values)
+
 
 for m in mach_values :
 
     tmp = flow1
-    tmp[0][0] = m * tmp[1]
+    tmp[0][0] = m * tmp[1] # m is vx, tmp[1] is a.
     x = oblique_limit(tmp, 10)
     p2 = x[2]
     pressure.append(p2)
@@ -25,11 +34,11 @@ for m in mach_values :
 
 
 print('Pressure: ', pressure)
-print('mach: ', generated_mach_values)
+print('mach: ', mach_values)
 
-plt.plot(pressure, generated_mach_values)
-plt.xlabel('Pressure')
-plt.ylabel('Mach ')
+plt.plot(mach_values, pressure)
+plt.xlabel('mach_values')
+plt.ylabel('Pressure ')
 plt.title('Pressure vs. Mach ')
 plt.grid(True)
 plt.show()
