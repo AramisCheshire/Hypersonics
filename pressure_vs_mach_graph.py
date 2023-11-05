e52086e0f908b60498a5b891591a30b071336624
testudoguy <149730227+testudoguy@users.noreply.github.com>
2023-11-05 14:43:22 +0200

    Update pressure_vs_mach_graph.py

--- pressure_vs_mach_graph.py
+++ pressure_vs_mach_graph.py
@@ -1,17 +1,19 @@
 import matplotlib.pyplot as plt
 from hyper import oblique_limit
+from hyper import oblique
 
 
 vel1 = [2444, 0, 0]
 flow1 = [vel1, 323, 132, 1.225, 200, 1.4]
 mach_values = []
 pressure = []
+density = []
 generated_mach_values = []
-tmp = []
+
 
 
 x = 0
-while x < 40 :
+while x < 10 :
       x = x + 1
       mach_values.append(x)
 
@@ -19,26 +21,39 @@ print('Initial_mach_values: ')
 print(mach_values)
 
 
+
+tmp = []
 for m in mach_values :
 
     tmp = flow1
     tmp[0][0] = m * tmp[1] # m is vx, tmp[1] is a.
     x = oblique_limit(tmp, 10)
-    p2 = x[2]
-    pressure.append(p2)
-    vx = x[0][0]
-    a = x[1]
-    mach = vx / a
-    generated_mach_values.append(mach)
+    y = oblique(tmp, 10)
 
+    p2 = x[2]
+    d = x[4]
+
+    density.append(d)
+    pressure.append(p2)
+
+#    vx = x[0][0]
+#    a = x[1]
+#    mach = vx / a
+#    generated_mach_values.append(mach)
 
 
 print('Pressure: ', pressure)
+
 print('mach: ', mach_values)
 
+print('density : ', density)
+
+
 plt.plot(mach_values, pressure)
+plt.plot(mach_values, density)
+
 plt.xlabel('mach_values')
-plt.ylabel('Pressure ')
-plt.title('Pressure vs. Mach ')
+plt.ylabel('Pressure (Blue) / Density (Orange)')
+plt.title('Pressure/Density vs. Mach ')
 plt.grid(True)
 plt.show()
